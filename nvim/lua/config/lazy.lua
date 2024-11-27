3980869f098518de75da502107a6eb60bfa1db9f
CodeTex <29424456+CodeTex@users.noreply.github.com>
2024-11-27 15:21:14 +0100

    ADD neotest extra

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with custom plugins
     { import = "plugins" },
   },
