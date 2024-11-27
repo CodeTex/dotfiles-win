3e8b29fd27685af5f1d3917ac0d123898da14795
CodeTex <29424456+CodeTex@users.noreply.github.com>
2024-11-27 15:12:14 +0100

    ADD tmux similar keymapping to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,15 +1,18 @@
 -- Initialize
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local opacity = 0.75
 
+--- Color Scheme
+config.color_scheme = "Catppuccin Mocha"
+
 -- Font
 config.font = wezterm.font_with_fallback({
-    {
-        family = "JetBrainsMono Nerd Font",
-        weight = "Regular",
-    },
-    "Segoe UI Emoji",
+	{
+		family = "JetBrainsMono Nerd Font",
+		weight = "Regular",
+	},
+	"Segoe UI Emoji",
 })
 config.font_size = 11
 
@@ -28,16 +31,125 @@ config.cursor_blink_rate = 250
 config.default_prog = { "pwsh", "-NoLogo" }
 
 -- Tabs
-config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = false
-config.show_tab_index_in_tab_bar = false
-config.use_fancy_tab_bar = true
+config.tab_and_split_indices_are_zero_based = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
 
 -- Keybindings
 config.keys = {
-    -- Remap paste for clipboard history compatibility
-    { key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+	-- Remap paste for clipboard history compatibility
+	{ key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
 }
+
+--- Tmux
+config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 2000 }
+config.keys = {
+	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		mods = "LEADER",
+		key = "b",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "LEADER",
+		key = "n",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "LEADER",
+		key = "|",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "LeftArrow",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "RightArrow",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "DownArrow",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "UpArrow",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+}
+
+-- leader + 0-9 to switch tabs
+for i = 1, 9 do
+	table.insert(config.keys, {
+		mods = "LEADER",
+		key = tostring(i),
+		action = wezterm.action.ActivateTab(i - 1), -- indexed tabs start from 0
+	})
+end
+
+-- tmux status
+wezterm.on("update-right-status", function(window, _)
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#cba6f7" } } -- Catppuccin Mocha Mauv
+	local prefix = ""
+
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f47e) -- space invader
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	end
+
+	-- arrow color based on if tab is not first pane
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#181825" } } -- Catppuccin Mocha Mantle
+	end
+
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b4befe" } }, -- Catppuccin Mocha Lavender
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
+end)
 
 -- Finish
 return config
+
