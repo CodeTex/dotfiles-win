fcce477961153f754dbb273e2f369bab9045d4e9
Julian <29424456+CodeTex@users.noreply.github.com>
2025-06-02 08:44:17 +0200

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,122 +1,141 @@
----@diagnostic disable:undefined-global
+--@diagnostic disable:undefined-global
 
--- Initialize
 local wezterm = require("wezterm")
+
 local config = wezterm.config_builder()
-local opacity = 0.75
+local opacity = 0.98
 
---- Color Scheme
-config.color_scheme = "Catppuccin Mocha"
-
--- Font
-config.font = wezterm.font_with_fallback({
-	{
-		family = "JetBrainsMono Nerd Font",
-		weight = "Regular",
-	},
-	"Segoe UI Emoji",
-})
-config.font_size = 11
-
--- Window
-config.initial_rows = 30
-config.initial_cols = 120
-config.window_decorations = "RESIZE"
-config.window_background_opacity = opacity
-config.window_close_confirmation = "NeverPrompt"
-config.win32_system_backdrop = "Acrylic"
-config.max_fps = 144
-config.animation_fps = 60
-config.cursor_blink_rate = 250
+-- Hyprland Compatibility
+config.enable_wayland = false
 
 -- Shell
 config.default_prog = { "pwsh", "-NoLogo" }
 
--- Tabs
+-- Appearance
+config.initial_rows = 40
+config.initial_cols = 120
+config.window_padding = {
+	left = 2,
+	right = 2,
+	top = 2,
+	bottom = 0,
+}
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.win32_system_backdrop = "Acrylic"
+config.window_background_opacity = opacity
+
+config.hide_mouse_cursor_when_typing = true
+config.max_fps = 144
+config.animation_fps = 60
+config.cursor_blink_rate = 250
+
+config.color_scheme = "Catppuccin Mocha"
+local color_mauve = "#cba6f7"
+local color_lavender = "#b4befe"
+local color_surface_2 = "#585b70"
+local color_surface_1 = "#45475a"
+local color_surface_0 = "#313244"
+local color_base = "#1e1e2e"
+local color_mantle = "#181825"
+local color_crust = "#11111b"
+
+-- Tab bar
+config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = false
+config.show_tab_index_in_tab_bar = false
 config.tab_and_split_indices_are_zero_based = false
-config.tab_bar_at_bottom = true
+config.tab_bar_at_bottom = false
+config.show_new_tab_button_in_tab_bar = false
 config.use_fancy_tab_bar = false
 
--- Keybindings
-config.keys = {
-	-- Remap paste for clipboard history compatibility
-	{ key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+config.tab_max_width = 35
+
+local leader_is_active = false
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local edge_background = color_crust
+	local background = color_surface_0
+	local foreground = color_lavender
+
+	if tab.is_active then
+		background = color_lavender
+		foreground = color_surface_0
+	elseif hover then
+		background = color_surface_1
+		foreground = color_lavender
+	end
+
+	local edge_foreground = background
+
+	local tab_left = wezterm.nerdfonts.ple_upper_right_triangle
+	local tab_right = wezterm.nerdfonts.ple_lower_left_triangle
+
+	if tab.tab_index == 0 then
+		tab_left = leader_is_active and wezterm.nerdfonts.ple_upper_right_triangle or ""
+	end
+
+	-- ensure that the titles fit in the available space, and that we have room for the edges
+	local title = tab.active_pane.title
+	title = wezterm.truncate_right(title, max_width - 2)
+
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = tab_left },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = " " .. title .. " " },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = tab_right },
+	}
+end)
+
+-- Font
+config.font = wezterm.font_with_fallback({
+	-- { family = 'FiraMono Nerd Font' },
+	{
+		family = "FiraCode Nerd Font",
+		weight = "Regular",
+		harfbuzz_features = {
+			-- https://github.com/tonsky/FiraCode/wiki/How-to-enable-stylistic-sets
+			"cv04", -- styles: i
+			"cv08", -- styles: l
+			"cv14", -- styles: r
+			"ss04", -- styles: $
+		},
+	},
+	{ family = "NotoSans Nerd Font" },
+	{ family = "JetBrains Nerd Font" },
+})
+config.font_size = 9
+
+config.warn_about_missing_glyphs = true
+
+config.harfbuzz_features = {
+	"kern", -- default kerning
+	"liga", -- default ligatures
+	"clig", -- default contextual ligatures
 }
 
---- Tmux
-config.leader = { key = "f", mods = "ALT", timeout_milliseconds = 2000 }
+-- Keybindings
+config.leader = { key = "f", mods = "ALT", timeout_milliseconds = 1000 }
 config.keys = {
-	{
-		mods = "LEADER",
-		key = "c",
-		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		mods = "LEADER",
-		key = "x",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-	{
-		mods = "LEADER",
-		key = "b",
-		action = wezterm.action.ActivateTabRelative(-1),
-	},
-	{
-		mods = "LEADER",
-		key = "n",
-		action = wezterm.action.ActivateTabRelative(1),
-	},
-	{
-		mods = "LEADER",
-		key = "|",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "LEADER",
-		key = "-",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "LEADER",
-		key = "h",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
-	{
-		mods = "LEADER",
-		key = "j",
-		action = wezterm.action.ActivatePaneDirection("Down"),
-	},
-	{
-		mods = "LEADER",
-		key = "k",
-		action = wezterm.action.ActivatePaneDirection("Up"),
-	},
-	{
-		mods = "LEADER",
-		key = "l",
-		action = wezterm.action.ActivatePaneDirection("Right"),
-	},
-	{
-		mods = "LEADER",
-		key = "LeftArrow",
-		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "RightArrow",
-		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "DownArrow",
-		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "UpArrow",
-		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
-	},
+	{ mods = "LEADER", key = "c", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ mods = "LEADER", key = "x", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+	{ mods = "LEADER", key = "b", action = wezterm.action.ActivateTabRelative(-1) },
+	{ mods = "LEADER", key = "n", action = wezterm.action.ActivateTabRelative(1) },
+	{ mods = "LEADER", key = "|", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ mods = "LEADER", key = "-", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ mods = "LEADER", key = "h", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ mods = "LEADER", key = "j", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ mods = "LEADER", key = "k", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ mods = "LEADER", key = "l", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ mods = "LEADER", key = "LeftArrow", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
+	{ mods = "LEADER", key = "DownArrow", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
+	{ mods = "LEADER", key = "UpArrow", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+	{ mods = "LEADER", key = "RightArrow", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
 }
 
 -- leader + 0-9 to switch tabs
@@ -130,28 +149,25 @@ end
 
 -- tmux status
 wezterm.on("update-right-status", function(window, _)
-	local SOLID_LEFT_ARROW = ""
+	local DIVIDER = ""
 	local prefix = ""
-	local ARROW_FOREGROUND = { Foreground = { Color = "#181825" } } -- Catppuccin Mocha Mantle
+	local ARROW_FOREGROUND = { Foreground = { Color = color_mantle } }
 
-	if window:leader_is_active() then
-		prefix = " " .. utf8.char(0x1f47e) -- space invader
-		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-	end
-
-	-- arrow color based on if tab is first pane
-	wezterm.log_info("tab id: " .. tostring(window:active_tab():tab_id()))
-	if window:active_tab():tab_id() == 0 then
-		ARROW_FOREGROUND = { Foreground = { Color = "#cba6f7" } } -- Catppuccin Mocha Mauve
+	leader_is_active = window:leader_is_active()
+	if leader_is_active then
+		prefix = " " .. wezterm.nerdfonts.md_space_invaders .. " " -- activation icon
+		DIVIDER = wezterm.nerdfonts.ple_lower_left_triangle
 	end
 
 	window:set_left_status(wezterm.format({
-		{ Background = { Color = "#b4befe" } }, -- Catppuccin Mocha Lavender
+		{ Background = { Color = color_mauve } },
+		{ Foreground = { Color = color_crust } },
 		{ Text = prefix },
 		ARROW_FOREGROUND,
-		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = color_mantle } },
+		{ Foreground = { Color = color_mauve } },
+		{ Text = DIVIDER },
 	}))
 end)
 
--- Finish
 return config
